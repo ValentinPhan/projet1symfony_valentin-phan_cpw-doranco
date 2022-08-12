6d1597a7f80f6882e9778e0973e2c5ad8f680677
Valentin <valentinnguyenphan@gmail.com>
2022-08-12 15:15:26 +0200

    Update 20220812_15:15

--- src/Controller/Projet1SymfonyController.php
+++ src/Controller/Projet1SymfonyController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Employes;
+use App\Form\EmployesFormType;
 use App\Repository\EmployesRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
